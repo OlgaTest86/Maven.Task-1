f165849e1414d8fe0c8f4e3a41f43228176826c0
Dudkina Olga <dudkinaolia@yandex.ru>
2021-11-12 19:56:30 +0300

    shouldCalculateNotRegisteredAndBonusOverLimit shouldCalculateNotRegisteredAndBonusUnderLimit

--- src/main/java/BonusServiceTest.java
+++ src/main/java/BonusServiceTest.java
@@ -1,4 +1,5 @@
 public class BonusServiceTest {
+
     public long calculate(long amount, boolean registered) {
         int percent = registered ? 3 : 1;
         long bonus = amount * percent / 100 / 100;
--- src/test/java/BonusServiceTestTest.java
+++ src/test/java/BonusServiceTestTest.java
@@ -5,13 +5,61 @@ import static org.junit.jupiter.api.Assertions.*;
 class BonusServiceTestTest {
 
     @Test
-    void calculate() {
+    void shouldCalculateRegisteredAndBonusUnderLimit() {
+        BonusServiceTest service = new BonusServiceTest();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateRegisteredAndBonusOverLimit() {
+        BonusServiceTest service = new BonusServiceTest();
+
+        // подготавливаем данные:
+        long amount = 1000_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateNotRegisteredAndBonusUnderLimit() {
         BonusServiceTest service = new BonusServiceTest();
 
         // подготавливаем данные:
         long amount = 1000_60;
         boolean registered = false;
-        long expected = 10;
+        long expected = 100;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateNotRegisteredAndBonusOverLimit() {
+        BonusServiceTest service = new BonusServiceTest();
+
+        // подготавливаем данные:
+        long amount = 100_000_60;
+        boolean registered = false;
+        long expected = 500;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
